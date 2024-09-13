530487b03efd2e8c096c9600938f809b56b79d35
doronseg <doron.segev1803@gmail.com>
2024-09-13 19:49:27 +0300

    test efficiency for SlashCommand.java

--- src/main/java/me/nerdoron/himyb/modules/bot/SlashCommand.java
+++ src/main/java/me/nerdoron/himyb/modules/bot/SlashCommand.java
@@ -2,19 +2,10 @@ package me.nerdoron.himyb.modules.bot;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 
 public abstract class SlashCommand {
     private static final Logger logger = LoggingHandler.logger(SlashCommand.class);
-
-    public void executeGlobal(SlashCommandInteractionEvent event) {
-        try {
-            execute(event);
-        } catch (Exception ex) {
-            logger.error(ExceptionUtils.getStackTrace(ex));
-        }
-    }
 
     public abstract void execute(SlashCommandInteractionEvent event);
 
