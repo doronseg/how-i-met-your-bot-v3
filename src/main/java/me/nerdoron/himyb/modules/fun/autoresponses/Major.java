9cd6d9ea214d2623be631d3ee85c4a584ff572dc
doronseg <doron.segev1803@gmail.com>
2024-09-28 13:59:04 +0300

    tryna spamthe server smh

--- src/main/java/me/nerdoron/himyb/modules/fun/autoresponses/Major.java
+++ src/main/java/me/nerdoron/himyb/modules/fun/autoresponses/Major.java
@@ -16,28 +16,31 @@ public class Major extends ListenerAdapter {
             String[] split = content.split("(?i)\\bmajor\\b ");
             if (split.length == 0) return;
             String after = split[1];
-            event.getMessage().reply("Major " + after + " \uD83E\uDEE1").queue();
-        }
-        if (event.getMessage().getContentDisplay().toLowerCase().contains("corporal ")) {
+            event.getMessage().reply("Major " + after + " <:tedsalute:884824742266290268>").queue();
+        } else if (event.getMessage().getContentDisplay().toLowerCase().contains("corporal ")) {
             String content = event.getMessage().getContentDisplay();
             String[] split = content.split("(?i)\\bcorporal\\b ");
             if (split.length == 0) return;
             String after = split[1];
-            event.getMessage().reply("Corporal " + after + " \uD83E\uDEE1").queue();
-        }
-        if (event.getMessage().getContentDisplay().toLowerCase().contains("private ")) {
+            event.getMessage().reply("Corporal " + after + " <:tedsalute:884824742266290268>").queue();
+        } else if (event.getMessage().getContentDisplay().toLowerCase().contains("private ")) {
             String content = event.getMessage().getContentDisplay();
             String[] split = content.split("(?i)\\bprivate\\b ");
             if (split.length == 0) return;
             String after = split[1];
-            event.getMessage().reply("Private " + after + " \uD83E\uDEE1").queue();
-        }
-        if (event.getMessage().getContentDisplay().toLowerCase().contains("general ")) {
+            event.getMessage().reply("Private " + after + " <:tedsalute:884824742266290268>").queue();
+        } else if (event.getMessage().getContentDisplay().toLowerCase().contains("general ")) {
             String content = event.getMessage().getContentDisplay();
-            String[] split = content.split("(?i)\\bgeneral\\b");
+            String[] split = content.split("(?i)\\bgeneral\\b ");
             if (split.length == 0) return;
             String after = split[1];
-            event.getMessage().reply("General " + after + " \uD83E\uDEE1").queue();
+            event.getMessage().reply("General " + after + " <:tedsalute:884824742266290268>").queue();
+        } else if (event.getMessage().getContentDisplay().toLowerCase().contains("colonel ")) {
+            String content = event.getMessage().getContentDisplay();
+            String[] split = content.split("(?i)\\bcolonel\\b ");
+            if (split.length == 0) return;
+            String after = split[1];
+            event.getMessage().reply("General " + after + " <:tedsalute:884824742266290268>").queue();
         }
 
     }
